e74ec08e4efb3247e731ade1526c275091a932a8
Danilo Bojanic <danilobojanic1@gmail.com>
2021-06-23 08:32:38 +0200

    Index implemented with redux

--- Festivals - front/src/index.js
+++ Festivals - front/src/index.js
@@ -1,50 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, HashRouter as Router, Switch } from 'react-router-dom';
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { HashRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { Navbar, Nav, Container,Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home';
-import {logout} from './services/auth';
-import Login from './components/login/Login';
+import Login from "./components/login/Login"
+import {logout} from './services/auth'
+// import Festival from './components/festival/Festival';
+// import CreateFestival from './components/festival/CreateFestival';
+// import Reservation from './components/reservation/Reservation;
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
-class App extends React.Component{
+
+
+
+class App extends React.Component {
+
     render() {
         return (
-            <div>
-                <Router>
-                    <Navbar expand bg="dark" variant="dark">
-                        <Navbar.Brand as={Link} to="/">
-                            Home
-                        </Navbar.Brand>
-                        <Nav>
-{/*                             <Nav.Link as={Link} to="/zadaci">Zadaci</Nav.Link>
-                            <Nav.Link as={Link} to="/stanja">Stanja</Nav.Link> */}
+            
+          <div>
+          <Router>
+            <Navbar expand bg="dark" variant="dark">
+              <Navbar.Brand as={Link} to="/">
+                  Home
+              </Navbar.Brand>
+             
+              
+              <Nav>
+                <Nav.Link as={Link} to="/festivali">
+                  Prikaz festivala
+                </Nav.Link>
+               
+                {window.localStorage['jwt'] ? 
+                          <Button onClick = {()=>logout()}>Logout</Button> :
+                          <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                          }
+              </Nav>
+            </Navbar>
+              <Container style={{paddingTop:"25px"}}>
+                <Switch>
+                  
+                <Route exact path="/login" component={Login}/>
+                {/* <Route exact path="/festival" component={Festival}/>
+                <Route exact path="/festival/create" component={CreateFestival}/>
+                <Route exact path="/festival/reserve/:id" component={Reservation}/> */}
 
-
-                            {window.localStorage['jwt'] ? 
-                            <Button onClick = {()=>logout()}>Logout</Button> :
-                            <Nav.Link as={Link} to="/login">Log in</Nav.Link>
-                            }
-                        </Nav>
-                    </Navbar> 
-                    <Container style={{paddingTop:"25px"}}>
-                    <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/login" component={Login}/>
-{/*                             <Route exact path="/zadaci" component={Zadatak} />
-                            <Route exact path="/zadaci/add" component={AddZadatak} />
-                            <Route exact path="/zadaci/edit/:id" component={EditZadatak} />
-                            <Route exact path="/stanja" component={Stanje} /> */}
-                    </Switch>
-                    </Container>
-                    </Router>
-            </div>
+                
+                  
+                  
+                </Switch>
+              </Container>
+            </Router>
+            
+          </div>
         )
     }
+};
 
-}
+let storeEnhancer = applyMiddleware(thunk);
 
 ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
+ <Provider store={createStore(reducers, storeEnhancer)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
